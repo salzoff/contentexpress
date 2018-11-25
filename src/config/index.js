72a4f30c8cadebf03b5282169f914e45e0543ed8
salzmann <marcus.salzmann@peakwork.com>
2018-11-25 19:07:45 +0100

    m

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,36 @@
-const path = require('path');
-try {
-    require(path.resolve(__dirname, '.env'));
-} catch(e) {
-    console.error('Error: No Environment configuration found!');
-    process.exit(1);
+let localConfig = false;
+const config = {
+    develop: {
+        giataBaseUrl: 'http://www.giata-xml.de',
+        giataUser: '',
+        giataPassword: '',
+        port: 7070,
+        defaultSearchShow: 'gid,hn,hk,sn,si,zn,zi,ln,lc,vc,vn,vid,oc,kn,katid,ks,ps,khid,kst,pics150,pics320,pics800,text,lk,cli,cld,ds,de,cv,vl,tt,ttc,katcode,geo,adr,fact'
+    },
+    production: {
+        giataBaseUrl: 'http://www.giata-xml.de',
+        giataUser: process.env.GIATA_USER,
+        giataPassword: process.env.GIATA_PASSWORD,
+        port: 7070,
+        defaultSearchShow: 'gid,hn,hk,sn,si,zn,zi,ln,lc,vc,vn,vid,oc,kn,katid,ks,ps,khid,kst,pics150,pics320,pics800,text,lk,cli,cld,ds,de,cv,vl,tt,ttc,katcode,geo,adr,fact'
+    }
+};
+
+let exportConfig = {};
+if (process.env.NODE_ENV === 'production') {
+    exportConfig = config.production;
+} else {
+    try {
+        localConfig = require('./local.config.js');
+    } catch(e) {}
+    if (localConfig) {
+        exportConfig = Object.assign(exportConfig, config.develop, localConfig.default);
+    } else {
+        exportConfig = Object.assign(exportConfig, config.develop, {
+            giataUser: process.env.GIATA_USER,
+            giataPassword: process.env.GIATA_PASSWORD
+        });
+    }
 }
 
-export default {
-    giataBaseUrl: 'http://www.giata-xml.de',
-    giataUser: process.env.GIATA_USER, //'132773',
-    giataPassword: process.env.GIATA_PASSWORD, //'nec69',
-    port: process.env.CONTENT_EXPRESS_PORT,
-    defaultSearchShow: 'gid,hn,hk,sn,si,zn,zi,ln,lc,vc,vn,vid,oc,kn,katid,ks,ps,khid,kst,pics150,pics320,pics800,text,lk,cli,cld,ds,de,cv,vl,tt,ttc,katcode,geo,adr,fact'
-};
+export default exportConfig;
---
+++ src/config/local.config.js
@@ -0,0 +1,4 @@
+export default {
+    giataUser: '132773',
+    giataPassword: 'nec69'
+}
